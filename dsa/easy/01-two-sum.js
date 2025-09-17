ac8dcffdaf0af61098544aaf269b8ec2e92df3a5
RenZz726 <rensonkroji02@gmail.com>
2025-09-17 11:33:14 +0530

    chore: initial repo structure

---
+++ dsa/easy/01-two-sum.js
@@ -0,0 +1,21 @@
+/*
+Problem: Two Sum
+Difficulty: Easy
+Approach:
+  - Use a hashmap to store values and indices.
+  - For each number, check if target - num exists.
+*/
+
+function twoSum(nums, target) {
+  const map = new Map();
+  for (let i = 0; i < nums.length; i++) {
+    const complement = target - nums[i];
+    if (map.has(complement)) {
+      return [map.get(complement), i];
+    }
+    map.set(nums[i], i);
+  }
+}
+
+// Test
+console.log(twoSum([2, 7, 11, 15], 9)); // [0,1]
